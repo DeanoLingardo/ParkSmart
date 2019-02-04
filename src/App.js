23490f04f3617655861117450190a42cf213bde0
deanolingardo <deanlingard@yahoo.co.uk>
2019-02-04 15:01:04 +0000

    Updated favicon

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import {
   Collapse,
@@ -56,7 +55,7 @@ export default class example extends React.Component {
                   </DropdownItem>
                   <DropdownItem divider />
                   <DropdownItem>
-                    Reset
+                    Logout
                   </DropdownItem>
                 </DropdownMenu>
               </UncontrolledDropdown>
