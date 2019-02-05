39d9afdd263fd9140858b80a0442088aa2cd763d
deanolingardo <deanlingard@yahoo.co.uk>
2019-02-05 16:08:33 +0000

    Jumbotron added to each page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,14 +1,34 @@
 // Home.js
 
 import React, { Component } from 'react';
+import {
+  Collapse,
+  Navbar,
+  Badge,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  Jumbotron,
+  Container,
+  UncontrolledDropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem,
+  Button } from 'reactstrap';
 
 class Home extends Component {
   render() {
     return (
-        <div>
-          <h2>Home</h2>
-          <h3>Welcome to the dashboard</h3>
-        </div>
+      <div>
+      <Jumbotron fluid>
+        <Container fluid>
+       <h1 className="display-3">Home</h1>
+       <p className="lead">This is the Home page.</p>
+     </Container>
+   </Jumbotron>
+ </div>
     );
   }
 }
--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,11 +1,32 @@
 import React, { Component } from 'react';
+import {
+  Collapse,
+  Navbar,
+  Badge,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  Jumbotron,
+  Container,
+  UncontrolledDropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem,
+  Button } from 'reactstrap';
 
 class Profile extends Component {
   render() {
     return (
-        <div>
-          <h2>Profile</h2>
-        </div>
+      <div>
+      <Jumbotron fluid>
+        <Container fluid>
+       <h1 className="display-3">Your Profile</h1>
+       <p className="lead">This is the profile page.</p>
+     </Container>
+   </Jumbotron>
+ </div>
     );
   }
 }
